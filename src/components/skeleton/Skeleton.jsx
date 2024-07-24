c8f5d2eed8cdb785d92e8e5145baffe8081ea999
Mr-AsimGhaffar <asim.ghaffar71@gmail.com>
2024-07-24 16:26:11 +0500

    add sorting by start date and end date

--- src/components/skeleton/Skeleton.jsx
+++ src/components/skeleton/Skeleton.jsx
@@ -2,7 +2,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function SkeletonCard() {
   return (
-    <div className="flex items-center justify-center">
+    <div className="flex items-center justify-center py-5">
       <div className="flex flex-col items-center space-y-3">
         <Skeleton className="h-[125px] w-[250px] rounded-xl bg-gradient-to-br from-blue-200 to-blue-300 animate-pulse" />
         <div className="space-y-2">
