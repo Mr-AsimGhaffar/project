bb41275ab694eeab8f9d24f639672a69d5a501e3
Mr-AsimGhaffar <asim.ghaffar71@gmail.com>
2024-09-20 16:57:59 +0500

    working

--- src/pages/propertyDetails/PopularityTrendGraph.jsx
+++ src/pages/propertyDetails/PopularityTrendGraph.jsx
@@ -35,74 +35,103 @@ export default function PopularityTrendGraph({
   const chartHeight = data.length * itemHeight + paddingHeight;
   return (
     <div>
-      <div>
-        <p className="font-montserrat text-[#0071BC] text-2xl font-bold p-2">
-          Trends - Most Searched Locations in {displayFirstName(location)}
-        </p>
-      </div>
-      <div>
-        <div className="flex flex-col md:flex-row p-2">
-          <div className="flex flex-col lg:flex-row justify-start md:justify-end gap-2 mb-4 md:mb-0">
-            {months.map((item) => (
-              <Button
-                key={item}
-                onClick={function () {
-                  setSelectedMonth(item);
-                }}
-              >
-                {formatDateToMonthYear(item)}
-              </Button>
-            ))}
-          </div>
+      <div className="hidden md:block">
+        <div>
+          <p className="font-montserrat text-[#0071BC] text-2xl font-bold p-2">
+            Trends - Most Searched Locations in {displayFirstName(location)}
+          </p>
         </div>
-        <div className="flex flex-col md:flex-row gap-4 font-inter text-sm">
-          <div className="w-full md:w-1/4 text-center flex flex-col gap-2 mb-4 md:mb-0">
-            <p>Rank</p>
-            {data.map((item, index) => (
-              <p key={index}>{index + 1}</p>
-            ))}
-          </div>
-          <div className="w-full md:w-1/4 flex flex-col gap-2 mb-4 md:mb-0 md:text-left text-center">
-            <p>LOCALITY</p>
-            {data.map((item) => (
-              <p key={item.id}>{item.title}</p>
-            ))}
-          </div>
-          <div className="w-full md:w-1/2">
-            <p>PERCENTAGE OF TOTAL SEARCHES (%)</p>
-            <div className="text-black">
-              <ResponsiveContainer width="100%" height={chartHeight}>
-                <BarChart
-                  layout="vertical"
-                  data={data.map((item) => ({
-                    name: item.search_percentage.toFixed(1),
-                    percentage: item.search_percentage,
-                  }))}
-                  margin={{
-                    top: 5,
-                    right: 30,
-                    left: 20,
-                    bottom: 5,
+        <div>
+          <div className="flex flex-col md:flex-row p-2">
+            <div className="flex flex-col lg:flex-row justify-start md:justify-end gap-2 mb-4 md:mb-0">
+              {months.map((item) => (
+                <Button
+                  key={item}
+                  onClick={function () {
+                    setSelectedMonth(item);
                   }}
                 >
-                  <CartesianGrid strokeDasharray="3 3" />
-                  <XAxis type="number" />
-                  <YAxis type="category" dataKey="name" />
-                  <Tooltip />
-                  <Legend />
-                  <Bar dataKey="percentage" fill="#0071BC" />
-                </BarChart>
-              </ResponsiveContainer>
+                  {formatDateToMonthYear(item)}
+                </Button>
+              ))}
             </div>
           </div>
-          <div className="w-full md:w-1/4 text-center flex flex-col gap-2">
-            <p>PERFORMANCE</p>
-            {data.map((item) => (
-              <p key={item.id}>
-                {performance(item.position, item.prev_position)}
-              </p>
-            ))}
+          <div className="flex flex-col md:flex-row gap-4 font-inter text-sm">
+            <div className="w-full md:w-1/4 text-center flex flex-col gap-2 mb-4 md:mb-0">
+              <p>Rank</p>
+              {data.map((item, index) => (
+                <p key={index}>{index + 1}</p>
+              ))}
+            </div>
+            <div className="w-full md:w-1/4 flex flex-col gap-2 mb-4 md:mb-0 md:text-left text-center">
+              <p>LOCALITY</p>
+              {data.map((item) => (
+                <p key={item.id}>{item.title}</p>
+              ))}
+            </div>
+            <div className="w-full md:w-1/2">
+              <p>PERCENTAGE OF TOTAL SEARCHES (%)</p>
+              <div className="text-black">
+                <ResponsiveContainer width="100%" height={chartHeight}>
+                  <BarChart
+                    layout="vertical"
+                    data={data.map((item) => ({
+                      name: item.search_percentage.toFixed(1),
+                      percentage: item.search_percentage,
+                    }))}
+                    margin={{
+                      top: 5,
+                      right: 30,
+                      left: 20,
+                      bottom: 5,
+                    }}
+                  >
+                    <CartesianGrid strokeDasharray="3 3" />
+                    <XAxis type="number" />
+                    <YAxis type="category" dataKey="name" />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="percentage" fill="#0071BC" />
+                  </BarChart>
+                </ResponsiveContainer>
+              </div>
+            </div>
+            <div className="w-full md:w-1/4 text-center flex flex-col gap-2">
+              <p>PERFORMANCE</p>
+              {data.map((item) => (
+                <p key={item.id}>
+                  {performance(item.position, item.prev_position)}
+                </p>
+              ))}
+            </div>
           </div>
+        </div>
+      </div>
+      <div className="p-2 block md:hidden">
+        <div>
+          <p>Trends</p>
+          <p>Trends-Most Searched Locations in</p>
+          <p>Percentage of Total Searches by Location</p>
+        </div>
+        <div className="flex items-center gap-2 overflow-x-auto">
+          {months.map((item) => (
+            <Button
+              key={item}
+              onClick={function () {
+                setSelectedMonth(item);
+              }}
+              className="rounded-3xl bg-gray-100 text-black"
+            >
+              {formatDateToMonthYear(item)}
+            </Button>
+          ))}
+        </div>
+        <div className="text-center">
+          {data.map((item) => (
+            <p className="rounded-md bg-gray-100 p-2 w-full" key={item.id}>
+              {item.title}
+            </p>
+          ))}
         </div>
       </div>
     </div>
