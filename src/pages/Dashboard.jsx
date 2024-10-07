78a08a82d439911ae616fb08bd498546ce115dc6
Mr-AsimGhaffar <asim.ghaffar71@gmail.com>
2024-10-07 16:27:12 +0500

    add color on loader

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -23,7 +23,7 @@ const Dashboard = ({
         setPropertyCategory={setPropertyCategory}
       />
       {loading ? (
-        <div className="flex justify-center py-10 lg:py-20">
+        <div className="flex justify-center py-10 lg:py-20 dark:bg-black">
           <div className="flex justify-center items-center">
             <div className="w-16 h-16 border-8 border-dashed rounded-full animate-spin border-blue-600"></div>
           </div>
